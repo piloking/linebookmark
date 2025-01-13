d08e142546675a4bf96cb7047876952a58fc54d1
piloking <141794311+piloking@users.noreply.github.com>
2025-01-13 14:38:20 +0900

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-const head: Record<string, string> = {
+const headers: Record<string, string> = {
     "content-type": "application/javascript",
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Headers":
@@ -10,12 +10,7 @@ Deno.serve(async (req) => {
     const url = new URL(req.url);
     if (url.pathname === "/load") {
         const res = await fetch("file:///src/load.js");
-        for (const key in head) {
-            if (Object.prototype.hasOwnProperty.call(head, key)) {
-                res.headers.set(key, head[key]);
-            }
-        }
-        return res;
+        return new Response(res.body, { headers });
     }
     return new Response("");
 });
